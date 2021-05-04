a7e744e36ee9bc9ff72ef7fc8871d98313d48185
CalDescent <>
2021-05-04 11:29:02 +0100

    Gracefully shut down nodes when using the --restart argument.

--- run.sh
+++ run.sh
@@ -31,6 +31,7 @@ while [ "${CURRENT_INSTANCE}" -lt "${INSTANCES}" ]; do
 
         if [ ! -z "${EXISTING_CONTAINER_ID}" ]; then
             echo "Stopping container ${INSTANCE_NAME} with ID ${EXISTING_CONTAINER_ID}..."
+            docker exec -it "${EXISTING_CONTAINER_ID}" ./stop.sh -t
             docker container stop "${EXISTING_CONTAINER_ID}"
             docker rm "${EXISTING_CONTAINER_ID}"
         fi
